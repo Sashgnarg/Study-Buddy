c40cda398f0dfb3965d853a933b7fb98e0f06d91
Seyed Nima Seifi Hessar <nimaseifi@Seyeds-MacBook-Pro.local>
2023-03-05 18:43:44 -0800

    added sections to form

--- StudyBuddyApp/src/app/app.module.ts
+++ StudyBuddyApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewUserComponent } from './new-user/new-user.component';
@@ -14,7 +14,8 @@ import { CourseSelectComponent } from './course-select/course-select.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- StudyBuddyApp/src/app/course-select/course-select.component.ts
+++ StudyBuddyApp/src/app/course-select/course-select.component.ts
@@ -1,4 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Course } from '../course';
+import { Section } from '../section';
+
 
 
 @Component({
@@ -8,14 +11,33 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class CourseSelectComponent {
   @Output() decrement = new EventEmitter();
-  public courses:any[]
+  public courses:Course[]
   @Input() num : number
+  public curCourse : string
+  public sections : Section[] | undefined
+
   constructor(){
     this.num=-1;
-    this.courses = [ {code : "cmpt295"} , {code: 'cmpt372'}];
+    let temp1 = new Course()
+    temp1.setCode('cmpt295')
+    temp1.addSection("d100" , '1:00' , '2:00')
+    temp1.addSection('d200' , '4:00' , '5:00')
+    let temp2 = new Course()
+    temp2.setCode('cmpt500');
+    temp2.addSection('d300' , '5:00' , '6:00')
+    temp2.addSection("d400" , "9:00" , "10:00");
+    this.courses = [temp1 , temp2]
+
+    this.curCourse = this.courses[0].getCode()
+    this.sections = this.courses.find( (e)=> e.getCode() == this.curCourse)?.getSections()
+
   }
 
   removeCourse(){
     this.decrement.emit(this.num);
+  }
+
+  setSection(){
+    this.sections = this.courses.find( (e)=> e.getCode() == this.curCourse)?.getSections()
   }
 }
