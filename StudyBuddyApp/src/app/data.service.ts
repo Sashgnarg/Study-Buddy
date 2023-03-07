e0725ba9cf8df72c486271fa42dd6c32bc08fb00
Seyed Nima Seifi Hessar <nimaseifi@Seyeds-MacBook-Pro.local>
2023-03-06 18:30:20 -0800

    test within dataService

--- StudyBuddyApp/src/app/data.service.ts
+++ StudyBuddyApp/src/app/data.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { User } from './user';
+const { Pool } = require('pg')
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  pool = new Pool({
+    connectionString: 'postgres://studybuddy:cmpt372@34.145.35.44'
+})
+
+
+  constructor() {
+
+    
+
+   }
+  test(){
+    this.pool.query("CREATE TABLE student (id SERIAL PRIMARY KEY,name VARCHAR(50),age INTEGER,courses TEXT[])")
+  }
+  createUser(U : User){
+
+  }
 }
