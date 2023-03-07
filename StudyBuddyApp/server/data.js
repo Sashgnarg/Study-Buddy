0c673e1437b1ff55609195e508fc043b4b5144fb
Adam Dee <adee@sfu.ca>
2023-03-06 19:41:34 -0800

    added database connection

---
+++ StudyBuddyApp/server/data.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: "postgres",
+    password: "cmpt372",
+    host: "34.145.35.44",
+    database: "postgres"
+})
+
+pool.connect();
+
+async function selectQuery() {
+    try {
+        var myquery = await pool.query("SELECT * FROM students")
+        console.log("1", myquery.rows)
+        pool.end()
+        process.exit()
+    }
+    catch (e) {
+        console.log("2", e)
+    }
+}
+
+selectQuery()
+
